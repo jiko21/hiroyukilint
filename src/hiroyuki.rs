cca27876a6733cb9955bb26516944353624855f9
jiko21 <daikikojima0531@icloud.com>
2021-06-03 00:39:21 +0900

    WIP: add checker

--- src/hiroyuki.rs
+++ src/hiroyuki.rs
@@ -6,32 +6,121 @@ use counted_array::counted_array;
 counted_array!(const WORD_LISTS: [&str; _] = [
     "絶対に",
     "確実に",
-    "必ず"
+    "必ず",
+    "明らかに",
 ]);
 
-pub fn check(text: String) -> bool {
-    for word in WORD_LISTS.iter() {
-        let re = Regex::new(word).unwrap();
-        println!("{}", text);
-        for matches in re.find_iter(&text) {
-            for i in 0..text.len() / 3 {
-                if i >=  matches.start() / 3 && i < matches.end() / 3 {
-                    print!("^");
-                } else {
-                    print!(" ");
-                }
-            }
+const BLANK: &str = " ";
+const CURSOR: &str = "^";
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum MissType {
+    Impression,
+    Lie,
+}
+
+fn get_miss_type(reason: &str) -> MissType {
+    match reason {
+        "明らかに" => MissType::Impression,
+        _ => MissType::Lie,
+    }
+}
+
+fn get_reason(miss_type: MissType) -> &'static str {
+    match miss_type {
+        MissType::Impression => &"明らかにって、それってあなたの感想ですよね?なんかデータとかあるんすか？",
+        _ => &"なんだろう、ウソつくのやめてもらってもいいっすか？",
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct CheckPoint {
+    line: usize,
+    start: usize,
+    end: usize,
+    miss_type: MissType,
+}
+
+impl CheckPoint {
+    fn new(line: usize, start: usize, end: usize, reason: &str) -> CheckPoint {
+        CheckPoint{
+            line,
+            start,
+            end,
+            miss_type: get_miss_type(reason),
         }
     }
-    true
+}
+
+pub fn check(line: usize, text: &String) -> Vec<CheckPoint> {
+    let mut point_vec: Vec<CheckPoint> = Vec::new();
+    for word in WORD_LISTS.iter() {
+        let re = Regex::new(word).unwrap();
+        for matches in re.find_iter(&text) {
+            let start = matches.start();
+            let end = matches.end();
+            let check_point = CheckPoint::new(line, start, end, word);
+            point_vec.push(check_point);
+        }
+    }
+    point_vec
+}
+
+pub fn print(line: usize, text: &String, check_point: CheckPoint) {
+    let _line = format!("line {}: ", line);
+    println!("{}{}", _line, text);
+    for _ in 0.._line.len() {
+        print!(" ");
+    }
+    let mut point_cursor = "".to_string();
+    let mut i = 0;
+    let mut cursor_at = 0;
+    while i < check_point.start {
+        if let Some(_) = text.get(i..(i+1)) {
+            i += 1;
+            cursor_at += 1;
+        } else {
+            i += 3;
+            cursor_at += 2;
+        }
+    };
+    point_cursor.push_str(&BLANK.to_string().repeat(cursor_at));
+    cursor_at = 0;
+    while i < check_point.end {
+        if let Some(_) = text.get(i..(i+1)) {
+            i += 1;
+            cursor_at += 1;
+        } else {
+            i += 3;
+            cursor_at += 2;
+        }
+    };
+    point_cursor.push_str(&CURSOR.to_string().repeat(cursor_at));
+    println!("{}", point_cursor);
+    println!("{}\n", get_reason(check_point.miss_type));
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::hiroyuki::CheckPoint;
+    use crate::hiroyuki::MissType;
     use crate::hiroyuki::check;
 
     #[test]
     fn test_check() {
-        assert_eq!(check("絶対に正しい絶対に、確実に失敗する".to_string()), true);
+        assert_eq!(check(11, &"絶対にa正しい明らかに".to_string()), vec![
+            CheckPoint {
+                line: 11,
+                start: 0,
+                end: 9,
+                miss_type: MissType::Lie,
+            },
+            CheckPoint {
+                line: 11,
+                start: 19,
+                end: 31,
+                miss_type: MissType::Impression,
+            },
+        ]);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ extern crate clap;
 extern crate exitcode;
 extern crate counted_array;
 
+use crate::hiroyuki::check;
+use crate::hiroyuki::print;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 use clap::{App, load_yaml};
@@ -16,13 +18,20 @@ fn main() {
     let f = File::open(file_path).expect("CANNOT OPEN FILE");
     let reader = BufReader::new(f);
 
+    let mut code_line = 1;
+    let mut error_count = 0;
     for line in reader.lines() {
         match line {
             Ok(line) => {
-
+                let points = check(code_line, &line);
+                error_count += points.len();
+                for point in points {
+                    print(code_line, &line, point);
+                }
+                code_line += 1;
             },
             Err(_) => {
-                eprintln!("Cannot read line @");
+                eprintln!("Cannot read line @ {}", code_line);
                 std::process::exit(exitcode::DATAERR);
             }
         };
