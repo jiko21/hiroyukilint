8cf8108da82499b4446865edef8d9a6932ee094f
jiko21 <daikikojima0531@icloud.com>
2021-06-03 23:47:49 +0900

    add Checker interface

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,22 @@
 #[macro_use]
 mod hiroyuki;
 extern crate clap;
-extern crate exitcode;
 extern crate counted_array;
+extern crate exitcode;
 
-use crate::hiroyuki::check;
-use crate::hiroyuki::print;
+use crate::hiroyuki::checker::Checker;
+use clap::{load_yaml, App};
 use std::fs::File;
-use std::io::{BufReader, BufRead};
-use clap::{App, load_yaml};
+use std::io::BufReader;
 
 fn main() {
-    let yaml = load_yaml!("../cli.yml");
+    let yaml = load_yaml!("./cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
     let file_path = matches.value_of("INPUT").unwrap();
 
     let f = File::open(file_path).expect("CANNOT OPEN FILE");
     let reader = BufReader::new(f);
 
-    let mut code_line = 1;
-    let mut error_count = 0;
-    for line in reader.lines() {
-        match line {
-            Ok(line) => {
-                let points = check(code_line, &line);
-                error_count += points.len();
-                for point in points {
-                    print(code_line, &line, point);
-                }
-                code_line += 1;
-            },
-            Err(_) => {
-                eprintln!("Cannot read line @ {}", code_line);
-                std::process::exit(exitcode::DATAERR);
-            }
-        };
-    }
+    let mut hiroyuki_checker = Checker::new(reader);
+    hiroyuki_checker.print();
 }
