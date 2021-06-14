6a8e9bd37ece9083be5ce13f80790e4b104f0bd0
jiko21 <daikikojima0531@icloud.com>
2021-06-14 23:21:18 +0900

    add args for unknown words list

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #[macro_use]
 mod hiroyuki;
+mod util;
 extern crate clap;
 extern crate counted_array;
 extern crate exitcode;
 
 use crate::hiroyuki::checker::Checker;
+use crate::util::util::read_file_to_array;
 use clap::{load_yaml, App};
 use std::fs::File;
 use std::io::BufReader;
@@ -13,10 +15,27 @@ fn main() {
     let yaml = load_yaml!("./cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
     let file_path = matches.value_of("INPUT").unwrap();
+    let blacklist_path = matches.value_of("BLACKLIST");
 
     let f = File::open(file_path).expect("CANNOT OPEN FILE");
     let reader = BufReader::new(f);
+    let mut blacklists = match blacklist_path {
+        Some(blacklist) => match File::open(blacklist) {
+            Ok(file) => {
+                let buf = BufReader::new(file);
+                read_file_to_array(buf)
+            }
+            Err(_) => {
+                vec![]
+            }
+        },
+        _ => {
+            vec![]
+        }
+    };
 
-    let mut hiroyuki_checker = Checker::new(reader);
+    println!("{:?}", blacklists);
+
+    let mut hiroyuki_checker = Checker::new(reader, &mut blacklists);
     hiroyuki_checker.print();
 }
